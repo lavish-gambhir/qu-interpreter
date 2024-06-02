98b26d196e8e5fed9a3822ea984ecdb8380f4918
Lavish Gambhir <lavish_gambhir@hotmail.com>
2024-06-02 19:15:42 +0530

    feat[repl]: add space after prompt

--- repl/repl.go
+++ repl/repl.go
@@ -9,7 +9,7 @@ import (
 	"github.com/lavish-gambhir/qu-interpreter/token"
 )
 
-const PROMPT = ">>"
+const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
