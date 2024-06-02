19259e88b9bc276be0b12d5699c6f708f094c55d
Lavish Gambhir <lavish_gambhir@hotmail.com>
2024-06-02 17:11:58 +0530

    read from stdin and print

--- repl/repl.go
+++ repl/repl.go
@@ -1,1 +1,29 @@
 package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/lavish-gambhir/qu-interpreter/lexer"
+	"github.com/lavish-gambhir/qu-interpreter/token"
+)
+
+const PROMPT = ">>"
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		lex := lexer.New(line)
+
+		for tok := lex.NextToken(); tok.Type != token.EOF; tok = lex.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
