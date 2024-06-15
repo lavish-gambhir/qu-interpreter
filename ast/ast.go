5fa9794a92aaca188ef48c28410129dbb0e0a075
Lavish Gambhir <lavish_gambhir@hotmail.com>
2024-06-15 11:36:37 +0530

    feat: init ast

---
+++ ast/ast.go
@@ -0,0 +1,45 @@
+package ast
+
+import "github.com/lavish-gambhir/qu-interpreter/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
+type ValStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (vs *ValStatement) statementNode()       {}
+func (vs *ValStatement) TokenLiteral() string { return vs.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
